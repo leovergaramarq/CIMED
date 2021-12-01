caac4a26cc2aa5a609ecc910c5078f7bda8f59c7
Leonardo Vergara <vergaradl@uninorte.edu.co>
2021-12-01 10:40:31 -0500

    addCitas

---
+++ src/public/js/ncita.js
@@ -0,0 +1,164 @@
+(() => {
+    // Calendar
+    let calendar = null;
+    document.addEventListener('DOMContentLoaded', function() {
+        const calendarEl = document.getElementById('calendar');
+    
+        calendar = new FullCalendar.Calendar(calendarEl, {
+            eventClick: function(info) {
+                console.log(info);
+            },
+            headerToolbar: {
+                center: 'addEventButton'
+            },
+            customButtons: {
+                addEventButton: {
+                    text: 'Agendar cita',
+                    click: () => openModal(),
+                }
+            },
+            initialView: 'dayGridMonth',
+            initialDate: '2021-12-01',
+            // headerToolbar: {
+                // left: 'prev,next today',
+                // center: 'title',
+                // right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            // },
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2021-12-01'
+                },
+                {
+                    title: 'Long Event',
+                    start: '2021-12-07',
+                    end: '2021-12-10'
+                },
+                {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-12-09T16:00:00'
+                },
+                {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-12-16T16:00:00'
+                },
+                {
+                    title: 'Conference',
+                    start: '2021-12-11',
+                    end: '2021-12-13'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2021-12-12T10:30:00',
+                    end: '2021-12-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2021-12-12T12:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2021-12-12T14:30:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2021-12-13T07:00:00'
+                },
+                {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2021-12-28'
+                }
+            ],
+        });
+    
+        calendar.render();
+    });
+
+    const $modal = document.querySelector('.modal');
+    const $mask = document.querySelector('#mask');
+    const $events = [...$modal.querySelectorAll('.modal-events__item')];
+
+    function closeModal() {
+        $modal.classList.add('hidden');
+        $mask.classList.add('hidden');
+    }
+
+    function openModal() {
+        $modal.classList.remove('hidden');
+        $mask.classList.remove('hidden');
+    }
+
+    document.addEventListener('keyup', e => {
+        window.location.pathname === '/ncita' && e.key === 'Escape' 
+            && ![...$modal.classList].includes('hidden') && closeModal();
+    });
+
+    $modal.querySelector('.modal__dismiss').addEventListener('click', () => closeModal());
+
+    $modal.querySelector('.modal__confirm').addEventListener('click', () => {
+        closeModal();
+
+        let $event = $events.find(e => [...e.classList].includes('sel'));
+        let doc = $event.querySelector('.modal-body__item__doc').textContent;
+        let esp = $event.querySelector('.modal-body__item__esp').textContent;
+        let date = $event.querySelector('.modal-body__item__date').textContent;
+        let start = $event.querySelector('.modal-body__item__start').textContent;
+        let end = $event.querySelector('.modal-body__item__end').textContent;
+        let place = $event.querySelector('.modal-body__item__place').textContent;
+
+        // console.log(doc, esp, date, start, end, place);
+        const hourToMilis = hour => {
+            let [hours, mins, rest] = hour.split(':');
+            hours = +hours;
+            mins = +mins;
+            if(rest.includes('p. m.')) hours = (hours + 12) % 24;
+            return ((hours - 5) * 3600 + mins * 60) * 1000;
+        }
+        date = new Date(date.split('/').reverse().join('/')).getTime();
+        start = new Date(date + hourToMilis(start)).toISOString();
+        end = new Date(date + hourToMilis(end)).toISOString();
+        date = new Date(date).toISOString();
+
+        calendar.addEvent({
+            title: esp,
+            start, end,
+            allDay: false,
+        });
+    });
+    
+    $events.forEach($event => {
+        $event.addEventListener('click', () => {
+            $event.classList.toggle('sel');
+            if([...$event.classList].includes('sel')) {
+                $events.forEach($event2 => {
+                    if($event !== $event2) {
+                        if([...$event2.classList].includes('sel')) {
+                            $event2.classList.remove('sel');
+                        }
+                    }
+                })
+            }
+        });
+    })
+
+    
+    
+    // Autocomplete
+    autocomplete(document.getElementById("input-doc"), ['Juan', 'Pedro', 'Juanita', 'Julián']);
+
+    // Submit form
+    const $form = document.querySelector('.modal-form form');
+    // $form.addEventListener('submit', event => {
+    //     if(!$form.checkValidity()) {
+    //         event.preventDefault()
+    //         event.stopPropagation()
+    //     }
+    //     $form.classList.add('was-validated')
+    // }, false);
+    $form.addEventListener('submit', e => {
+        e.preventDefault();
+    })
+})();
--- src/routes/index.js
+++ src/routes/index.js
@@ -35,8 +35,57 @@ router.get('/miscitas', isLoggedIn, (req, res) => {
 });
 
 router.get('/ncita', isLoggedIn, (req, res) => {
+    const events = [
+        {
+            id: '01',
+            place: 'Cra 12 # 8B - 19',
+            doctor: {
+                name: 'Jaime Altozano',
+                esp: 'Otorrinolaringólogo',
+            },
+            start: new Date(2021, 12-1, 19, 16, 00),
+            end: new Date(2021, 12-1, 19, 16, 30),
+        },
+        {
+            id: '02',
+            place: 'Cra 6 # 61 - 22',
+            doctor: {
+                name: 'Mario Mandzukic',
+                esp: 'Pediatra',
+            },
+            start: new Date(2021, 12-1, 19, 11, 30).toString(),
+            end: new Date(2021, 12-1, 19, 12, 30).toString(),
+        },
+        {
+            id: '03',
+            place: 'Cra 6A # 54 - 32',
+            doctor: {
+                name: 'José Guardiola',
+                esp: 'Cirujano',
+            },
+            start: new Date(2021, 12-1, 11, 6, 30).toString(),
+            end: new Date(2021, 12-1, 11, 8, 30).toString(),
+        },
+        {
+            id: '04',
+            place: 'Cra 46 # 9 - 16',
+            doctor: {
+                name: 'Miriam Campos',
+                esp: 'Ortopedista',
+            },
+            start: new Date(2021, 12-1, 31, 23, 30).toString(),
+            end: new Date(2021, 12-1, 31, 23, 45).toString(),
+        },
+    ]
+    events.sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
+    
+    events.forEach(e => {
+        e.date = new Date(e.start).toLocaleDateString();
+        e.start = new Date(e.start).toLocaleTimeString();
+        e.end = new Date(e.end).toLocaleTimeString();
+    })
 
-    res.render('userSession/ncita');
+    res.render('userSession/ncita', {events});
 });
 
 router.get('/admin', isLoggedIn, (req, res) => {
